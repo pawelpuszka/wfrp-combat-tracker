625861e17098c5c7fa74752eca444337fd0de552
Pawel Puszka <pawel.puszka@gmail.com>
2026-09-10 00:34:18 +0200

    exception handling, renaming functions

--- backend/api/routes/characters.py
+++ backend/api/routes/characters.py
@@ -1,9 +1,23 @@
 from fastapi import APIRouter
+from pathlib import Path
+from  fastapi import HTTPException
 from backend.services.character_service import CharacterService
+from backend.schemas.character_card import CharacterCard 
 
 router = APIRouter()
 
 @router.get("/")
-def list_characters():
+def list_characters() -> dict:
     service = CharacterService()
     return {"characters": service.list_characters("players")}
+
+
+@router.get("/{id}")
+def get_full_character_card(id: str, card_type="players") -> CharacterCard:
+    file_name: str = str(id) + ".json"
+    path_to_file: Path = Path("data/characters") / card_type/ file_name
+    service = CharacterService()
+    try:
+        return service.read_character_from_json(path_to_file=path_to_file)
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Postać nie istnieje")
--- backend/services/character_service.py
+++ backend/services/character_service.py
@@ -5,14 +5,16 @@ from backend.schemas.character_card import CharacterCard
 
 class CharacterService():
 
-    def load_character(self, path_to_file: str):
+    def read_character_from_json(self, path_to_file: str):
+        if not path_to_file.exists():
+            raise FileNotFoundError(f"Plik {path_to_file} nie istnieje.")
         with open(path_to_file, 'r', encoding='utf-8') as file:
             data = json.load(file)
         return CharacterCard.model_validate(data)
 
 
-    def upload_character(self, data: CharacterCard, card_type: str="players"):
-        file_name: str = data.name + ".json"
+    def upload_character_to_json(self, data: CharacterCard, card_type: str="players"):
+        file_name: str = str(data.id) + ".json"
         path_to_file = Path("data/characters/") / card_type / file_name
         with open(path_to_file, 'w', encoding="utf-8") as file:
             file.write(data.model_dump_json(indent=4))
@@ -33,9 +35,9 @@ class CharacterService():
 
 if __name__ == "__main__":
     service = CharacterService()
-    character = service.load_character("data/characters/players/brunon_witz_test.json")
+    character = service.read_character_from_json("data/characters/players/brunon_witz_test.json")
     # print(character.model_dump_json(indent=4))
 
-    service.upload_character(character)
+    service.upload_character_to_json(character)
     characters = service.list_characters("players")
     print(characters)
